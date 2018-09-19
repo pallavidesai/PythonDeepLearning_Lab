815f2254aad5e0a6e2927e214eb21746a04c9f5d
pallavidesai <s.pallavidesai@gmail.com>
2018-09-19 13:34:13 -0500

    non repeat

--- Lab1/source/NonRepeating.py
+++ Lab1/source/NonRepeating.py
@@ -1,7 +1,8 @@
 # Initialize a dictionary
 CountDictionary = {}
 # Get Input from User
-MyString = input("Please enter you string")
+MyStringInp = input("Please enter you string")
+MyString = ''.join(MyStringInp.split())
 for letter in MyString:
     # If letter is repeating
     if letter in CountDictionary:
@@ -12,9 +13,14 @@ for letter in MyString:
     else:
         # word's first time occurrence
         CountDictionary[letter] = 1
+checker = 0
 for letter, frequency in CountDictionary.items():
     # Check if a letter has occurred only once and come out of loop
     if frequency == 1:
-        print("first non-repeated characters in you string is ", letter)
+        print("first non-repeated characters in your string is ", letter)
+        checker = 1
         break
+
+if checker == 0:
+    print("There are no non-repeated characters in your string")
 
